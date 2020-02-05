07f5e7a72f6dd9bbaf4fb58012027f5bf71d410e
vzwGrey <vzwgrey@gmail.com>
2020-02-05 01:01:21 +0100

    feat: max-depth option

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ use tree::util::IdentifyLast;
 struct Opts {
     #[structopt(name = "ROOT")]
     root: PathBuf,
+
+    #[structopt(short = "d", long = "max-depth")]
+    max_depth: Option<usize>
+}
+
+impl Opts {
+    fn max_depth_reached(&self, depth: usize) -> bool {
+        self.max_depth
+            .map(|max_depth| depth >= max_depth)
+            .unwrap_or(false)
+    }
 }
 
 fn main() {
@@ -15,13 +26,13 @@ fn main() {
         eprintln!("tree: {} is not a directory.", opts.root.display());
     } else {
         println!("{}", opts.root.display());
-        print_dir(opts.root.as_path(), String::from("")).unwrap();
+        print_dir(opts.root.as_path(), String::from(""), &opts).unwrap();
     }
 }
 
 /// Recursively traverse directories, printing each entry
 /// in a tree-like structure.
-fn print_dir(dir: &Path, indent: String) -> io::Result<()> {
+fn print_dir(dir: &Path, indent: String, opts: &Opts) -> io::Result<()> {
     for (is_last, entry) in fs::read_dir(dir)?.identify_last() {
         let path = entry?.path();
         let branch = if is_last {
@@ -30,13 +41,14 @@ fn print_dir(dir: &Path, indent: String) -> io::Result<()> {
             "|---"
         };
         println!("{}{}{}", indent, branch, path.file_name().unwrap().to_str().unwrap());
-        if path.is_dir() {
+        if path.is_dir() && !opts.max_depth_reached(indent.len() / 4) {
             let new_indent = if is_last {
                 "    "
             } else {
                 "|   "
             };
-            print_dir(&path, format!("{}{}", indent, new_indent))?;
+
+            print_dir(&path, format!("{}{}", indent, new_indent), opts)?;
         }
     }
 
